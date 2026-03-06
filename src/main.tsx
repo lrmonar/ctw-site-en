87a1fb4d48e779547fb52e763949b90b68f26dd2
lrmonar <luismonar@gmail.com>
2026-03-06 15:43:48 -0500

    Fix React root initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import "./i18n";
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
