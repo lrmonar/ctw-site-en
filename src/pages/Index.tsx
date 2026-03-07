f55afa874d45a21f567c01dac9d6b91e6b75eec5
lrmonar <luismonar@gmail.com>
2026-03-07 18:32:04 -0500

    fix index.tsx by incrementally adding NewsPanel component

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,12 @@
 import NewsCarousel from '@/components/NewsCarousel';
+import NewsPanel from '@/components/NewsPanel';
 
 const Index = () => {
   return (
     <main>
       <h1>Test Page</h1>
 	  <NewsCarousel />
+	  <NewsPanel title="Test" category="cellular" />
     </main>
   );
 };
